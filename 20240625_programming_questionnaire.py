108dc4a2312a5d72db5da252499a778826ed5ffa
jgarcia1127 <jcgsah@gmail.com>
2024-06-27 14:05:26 -0400

    Hardware Software Project 2

---
+++ 20240625_programming_questionnaire.py
@@ -0,0 +1,11 @@
+def main():
+    name = "Professor Nedd"
+    print("We want to know if you like programming!")
+    print()
+    print("Do you like programming "+ name + "?")
+    answer = input()
+    print("Great! You said" + answer + "?")
+    print("Let's start learning some python today")
+
+if __name__== "__main__":
+    main()
